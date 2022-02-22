942fc729861f89e7a6ce033fef8243d70318c74b
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2022-02-22 13:13:22 +0530

    Added music switching trigger

--- immerser.py
+++ immerser.py
@@ -4,57 +4,67 @@ import pyHook
 import time
 import threading
 
-isTyping = False
-immerseCount = 0
-
 def OnKeyEvent(event):
-    global isTyping
-    isTyping = True
+    global typeCount
+    typeCount += 1
     return True
 
-# Check every 0.5 seconds where user is typing
-def setTypng():
-    global isTyping
+def isTyping():
+    global keysPer
     while True:
-        isTyping = False
-        time.sleep(0.5)
+        keysPer = typeCount - isTyping.oldCount
+        isTyping.oldCount = typeCount
+        time.sleep(1)
 
-# Set immerse count, this is used to intense/mild the immersive music
-def checkTyping():
-    global sound_main
-    global sound_intense
-    global immerseCount
+# Play switch between immersive and normal music
+def adaptMusic():
+    global switching
+
     while True:
-        if (isTyping == True):
-            print("Immerse")
-            immerseCount += 1
-            # if (sounda.get_volume() != 0.1 and soundb.get_volume() != 1):
-            # for i in range(1, 9):
-            #     sounda.set_volume(i/10)
-            #     soundb.set_volume((10-i)/10)
-            #     time.sleep(0.5)
+        print(round(sound_intense.get_volume(),1), round(sound_main.get_volume(),1))
+        if ( ((adaptMusic.prevCount == 0 and keysPer > 3) or (keysPer == 0 and adaptMusic.prevCount > 0)) and switching == False ):
+            print("Switch")
+            switching = True
+            # switchMusic()
+            time.sleep(2)
+            switching = False
         else:
-            print("No immerse")
-            immerseCount -= 1
-            # if (sounda.get_volume() != 1 and soundb.get_volume() != 0.1):
-            # for i in range(1, 9):
-            #     sounda.set_volume((10-i)/10)
-            #     soundb.set_volume(i/10)
-            #     time.sleep(0.5)
-        time.sleep(3)
+            print("Normal")
 
+        adaptMusic.prevCount = keysPer
+        time.sleep(1)
+
+def switchMusic():
+    global sound_intense
+    global sound_main
+
+    if (round(sound_intense.get_volume(),1) == 0.1 and sound_main.get_volume() == 1):
+        for i in range(1,10):
+            sound_intense.set_volume(i/10)
+            sound_main.set_volume((10-i)/10)
+    elif (sound_intense.get_volume() == 1 and round(sound_main.get_volume(),1) == 0.1):
+        for i in range(0,10):
+            sound_intense.set_volume((10-i)/10)
+            sound_main.set_volume(i/10)
+
+isTyping.oldCount = 0
+typeCount = 0
+switching = False
+keysPer = 0
+adaptMusic.prevCount = 0
 
 pygame.init()
 pygame.mixer.init()
 sound_main = pygame.mixer.Sound("main.mp3")
 sound_intense = pygame.mixer.Sound("intense.wav")
+sound_intense.set_volume(0.1)
 sound_main.play(-1)
 sound_intense.play(-1)
 
-threading.Thread(target=checkTyping).start()
-threading.Thread(target=setTypng).start()
+threading.Thread(target=isTyping).start()
+threading.Thread(target=adaptMusic).start()
 
 hookManager = pyHook.HookManager()
-hookManager.KeyAll = OnKeyEvent
+hookManager.KeyUp = OnKeyEvent
 hookManager.HookKeyboard()
 pythoncom.PumpMessages()
