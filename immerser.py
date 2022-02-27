b5bfdca8bf317915c10055a608c1776843eb17de
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2022-02-27 20:59:43 +0530

    Turning to OOP

--- immerser.py
+++ immerser.py
@@ -10,26 +10,26 @@ def OnKeyEvent(event):
     return True
 
 def isTyping():
-    global keysPer
-    while True:
-        keysPer = typeCount - isTyping.oldCount
-        isTyping.oldCount = typeCount
-        time.sleep(1)
+    keysPer = typeCount - isTyping.oldCount
+    isTyping.oldCount = typeCount
+    return keysPer
 
 # Play switch between immersive and normal music
 def adaptMusic():
     global switching
 
     while True:
-        print(round(sound_intense.get_volume(),1), round(sound_main.get_volume(),1))
-        if ( ((adaptMusic.prevCount == 0 and keysPer > 3) or (keysPer == 0 and adaptMusic.prevCount > 0)) and switching == False ):
-            print("Switch")
+        # print(round(sound_intense.get_volume(),1), round(sound_main.get_volume(),1))
+        keysPer = isTyping()
+        # print(adaptMusic.prevCount, keysPer)
+        if ( ((adaptMusic.prevCount <= 2 and adaptMusic.prevCount >= 0 and keysPer > 2) or (keysPer <= 2 and keysPer >= 0 and adaptMusic.prevCount > 2)) and switching == False ):
+            # print("Switch")
             switching = True
-            # switchMusic()
-            time.sleep(2)
+            switchMusic()
             switching = False
         else:
-            print("Normal")
+            a = 0
+            # print("Normal")
 
         adaptMusic.prevCount = keysPer
         time.sleep(1)
@@ -38,30 +38,35 @@ def switchMusic():
     global sound_intense
     global sound_main
 
-    if (round(sound_intense.get_volume(),1) == 0.1 and sound_main.get_volume() == 1):
-        for i in range(1,10):
+    if (round(sound_intense.get_volume(),1) == 0 and sound_main.get_volume() == 1):
+        # print("Intense: %f -> %f | Main: %f -> %f" % (round(sound_intense.get_volume(),1), 1, round(sound_main.get_volume(),1), 0))
+        for i in range(0,11):
             sound_intense.set_volume(i/10)
             sound_main.set_volume((10-i)/10)
-    elif (sound_intense.get_volume() == 1 and round(sound_main.get_volume(),1) == 0.1):
-        for i in range(0,10):
+            # print("\tIntense: %f | Main: %f" % (round(sound_intense.get_volume(),1), round(sound_main.get_volume(),1)))
+            time.sleep(0.5)
+    elif (sound_intense.get_volume() == 1 and round(sound_main.get_volume(),1) == 0):
+        # print("Intense: %f -> %f | Main: %f -> %f" % (round(sound_intense.get_volume(),1), 0, round(sound_main.get_volume(),1), 1))
+        for i in range(0,11):
             sound_intense.set_volume((10-i)/10)
             sound_main.set_volume(i/10)
+            # print("\tIntense: %f | Main: %f" % (round(sound_intense.get_volume(),1), round(sound_main.get_volume(),1)))
+            time.sleep(0.2)
 
-isTyping.oldCount = 0
 typeCount = 0
 switching = False
 keysPer = 0
 adaptMusic.prevCount = 0
+isTyping.oldCount = 0
 
 pygame.init()
 pygame.mixer.init()
-sound_main = pygame.mixer.Sound("main.mp3")
-sound_intense = pygame.mixer.Sound("intense.wav")
-sound_intense.set_volume(0.1)
+sound_main = pygame.mixer.Sound("Mixing\opgx\main.mp3")
+sound_intense = pygame.mixer.Sound("Mixing\opgx\intense.wav")
+sound_intense.set_volume(0)
 sound_main.play(-1)
 sound_intense.play(-1)
 
-threading.Thread(target=isTyping).start()
 threading.Thread(target=adaptMusic).start()
 
 hookManager = pyHook.HookManager()
